84e2645d587d96a28f78884c52511ae1f83852d2
mahmoodxi <36129031+mahmoodxi@users.noreply.github.com>
2021-06-06 03:59:16 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+apt-get update
+apt-get install python3-pip git -y
+pip3 install requests flask names
+git clone https://github.com/mahmoodxi/Tele /var/MTProxy
+chmod +x /var/MTProxy/objs/bin/mtproto-proxy
+wget https://raw.githubusercontent.com/mahmoodxi/Tlx/main/run.py -P /root
+wget https://raw.githubusercontent.com/mahmoodxi/Tlx/main/run.service -P /etc/systemd/system
+systemctl daemon-reload
+systemctl restart run.service
+systemctl enable run.service
